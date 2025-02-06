55a4fd3aade7e4f28f6ba502390b5dbb350bf92e
Alan Garber <a8garber@Alans-MacBook-Air.local>
2025-02-06 11:43:54 -0500

    first commit; docker and prisma set up and seeded, first migration took place

---
+++ prisma/migrations/20250206163509_first_migration/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "Survey" (
+    "id" TEXT NOT NULL,
+    "question" TEXT NOT NULL,
+
+    CONSTRAINT "Survey_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Answer" (
+    "id" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    "surveyId" TEXT NOT NULL,
+
+    CONSTRAINT "Answer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Survey_question_key" ON "Survey"("question");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Answer_answer_key" ON "Answer"("answer");
+
+-- AddForeignKey
+ALTER TABLE "Answer" ADD CONSTRAINT "Answer_surveyId_fkey" FOREIGN KEY ("surveyId") REFERENCES "Survey"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
