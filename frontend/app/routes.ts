9c24f4fa2dbe75e0e9aa2ea4c86ece6fb9872181
Alan Garber <a8garber@Alans-MacBook-Air.local>
2025-02-06 16:43:04 -0500

    more frontend stuff

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -5,12 +5,12 @@ import {
   route,
 } from "@react-router/dev/routes";
 
-// export default [index("routes/home.tsx")] satisfies RouteConfig;
-
 export default [
   index("routes/home.tsx"),
   ...prefix("/surveys", [
     index("routes/surveys/index.tsx"),
     route("/new", "routes/surveys/new.tsx"),
+    route(":id", "routes/surveys/survey.tsx"),
+    route(":id/results", "routes/surveys/results.tsx"),
   ]),
 ] satisfies RouteConfig;
